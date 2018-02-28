5edadc6d9238ee3dd7e0f999a41089676dee6313
sharanraj1996 <36887947+sharanraj1996@users.noreply.github.com>
2018-02-28 22:46:07 +0530

    Add files via upload

---
+++ t/tensor.php
@@ -0,0 +1,60 @@
+<?php
+
+$target_dir = "uploads/";
+
+
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+$uploadOk = 1;
+
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+// Check if image file is a actual image or fake 
+image
+if(isset($_POST["submit"])) 
+{
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+ 
+   if($check !== false)
+ {
+        //echo "File is an image - " . $check["mime"] . ".";
+
+        $uploadOk = 1;
+    }
+ else {
+        echo "File is not an image.";
+
+        $uploadOk = 0;
+    }
+}
+
+// Check if $uploadOk is set to 0 by an error
+
+if ($uploadOk == 0) 
+{
+    echo "Sorry, your file was not uploaded.";
+
+// if everything is ok, try to upload file
+} 
+else
+ {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file))
+ {
+     
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+
+    }
+}
+
+
+$r = shell_exec("sudo /home/cse/t.sh $target_file 2>&1");
+
+$t = file_get_contents("/home/cse/r.txt");
+preg_match("/(ga.*|mo.*)/",$t,$matches);
+
+echo $matches[0];
+
+ 
+?>
+
---
+++ t/tensor.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+cd /home/cse/tensorflow
+
+
+sudo bazel-bin/tensorflow/examples/label_image/label_image --graph=/home/cse/Desktop/output_graph.pb --labels=/home/cse/Desktop/output_labels.txt --output_layer=final_result --image=/var/www/html/$1 --input_layer=Mul 2> /home/cse/r.txt 
+
+
+
